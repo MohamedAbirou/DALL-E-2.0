b4478eeaba69be1523d32e5478f81b73285cffc4
Mohamed Abirou <109366637+MohamedAbirou@users.noreply.github.com>
2024-01-16 18:01:09 +0100

    Update CreatePost.jsx

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://dall-e-2-0-1zcd.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
